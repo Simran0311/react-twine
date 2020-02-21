47cc07817b2d95d6158770ded3ad3672edcfc686
Simran0311 <61179956+Simran0311@users.noreply.github.com>
2020-02-21 17:45:13 +0530

    Update Form.js

--- src/Form.js
+++ src/Form.js
@@ -86,8 +86,8 @@ export default class Form extends React.Component {
           console.log(user);
 
           if(this.state.final_name !== ""&& this.state.final_email !== "" && this.state.final_title !== "" && this.state.final_techStack !== "" && this.state.final_message !== ""){     
-          console.log("submit");
-              this.submit();
+                console.log("submit");
+              $("form").submit();
           }
           else
           {
