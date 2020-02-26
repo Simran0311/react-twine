fec8bcf64e48e4e51073ee9646fb70233c802a04
Simran0311 <61179956+Simran0311@users.noreply.github.com>
2020-02-26 12:53:24 +0530

    Update Careers.js

--- src/Careers.js
+++ src/Careers.js
@@ -9,10 +9,10 @@ export default class OurStory extends React.Component {
       super(props);
    }
    componentDidMount() {
-      
+      window.scrollTo(0, 0)
        this.loadGreenhouseJobs(() => {
          window.Grnhse.Iframe.load();
-          window.scrollTo(0, 0)
+          
       });  
    }
 
